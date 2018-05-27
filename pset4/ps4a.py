eefe8caeb2dc3f4619b6efc9dac4bc6ba98fc2e8
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-05-27 14:36:58 +0400

    Finished problem 4.5

--- pset4/ps4a.py
+++ pset4/ps4a.py
@@ -234,31 +234,36 @@ def playHand(hand, wordList, n):
     # Keep track of the total score
     score = 0
     # As long as there are still letters left in the hand:
-
+    while calculateHandlen(hand) != 0:
         # Display the hand
-
+        print('Current Hand:', end="  ")
+        displayHand(hand)
         # Ask user for input
-
+        guessed_word = input('Enter word, or a "." to indicate that you are finished:')
         # If the input is a single period:
-
+        if guessed_word is '.':
             # End the game (break out of the loop)
-
+            break
 
         # Otherwise (the input is not a single period):
-
+        else
             # If the word is not valid:
-
+            if not isValidWord(guessed_word, hand, wordList):
                 # Reject invalid word (print a message followed by a blank line)
-
+                print('Invalid word, please try again.')
             # Otherwise (the word is valid):
-
+            else
                 # Tell the user how many points the word earned, and the updated total score, in one line followed by a blank line
-
+                score += getWordScore(guessed_word, n)
+                print('"' + guessed_word + '"', 'earned', getWordScore(guessed_word, n), 'points.', 'Total:', score, 'points')
                 # Update the hand
-
+                hand = updateHand(hand, guessed_word)
 
     # Game is over (user entered a '.' or ran out of letters), so tell user the total score
-
+    if guessed_word is '.':
+        print('Goodbye! Total score:', score, 'points')
+    else:
+        print('Run out of letters. Total score:', score, 'points. ')
 
 #
 # Problem #5: Playing a game
